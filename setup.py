236876d1cf60d6ed31aecab96e5b32fca53bd194
Guy Avraham <guyavrah1986@gmail.com>
2019-09-28 18:39:50 +0300

    SETUP_PY:adding the usage in the exclude argument within the find_packages() method in order to not take into account the tests package (which is used for testing and on-going development purposes).

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name="scapywrapper",
       author="Guy Avraham",
       author_email="guyavrah1986@gmail.com",
       license="GPL-2.0",
-      packages=find_packages(),
+      packages=find_packages(exclude=["tests"]),
       include_package_data=True,
       install_requires=["scapy"])
