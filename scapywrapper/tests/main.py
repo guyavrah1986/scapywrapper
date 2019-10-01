1a831a838b9df1113549abe9d1ab99416af8dd2c
Guy Avraham <guyavrah1986@gmail.com>
2019-10-01 12:27:51 +0300

    MAIN:some testing in the main function.

--- scapywrapper/tests/main.py
+++ scapywrapper/tests/main.py
@@ -1,9 +1,11 @@
 import os
 import sys
 
+from scapy.utils import PcapReader
 from scapywrapper.utilities.loggable import Loggable
 from scapywrapper.pcapFileParser.pcapFileParser import PcapFileParser
 from scapywrapper.packetParser.igpPacketParser import IgpPacketParser
+from scapywrapper.packets.basePacket import BasePacket
 
 class MainClass(Loggable):
 
@@ -26,6 +28,11 @@ class MainClass(Loggable):
         igp_packet_parser = IgpPacketParser()
         return igp_packet_parser.igp_packet_parser_get_packet_protocol_type(packet_data)
 
+    def get_packet_time(self, packet_data):
+        base_packet = BasePacket(packet_data)
+        packet_time = base_packet.get_packet_time()
+        print(self.log_me() + "packet time is:" + str(packet_time))
+
 
 if __name__ == "__main__":
     func_name = "main - "
@@ -34,9 +41,12 @@ if __name__ == "__main__":
     print(func_name + "got command line arguments:\n" + str(sys.argv))
     pcap_file_parser = main_obj.create_pcap_file_parser_for_pcap_file(sys.argv[1])
     packet_num = int(sys.argv[2])
-    packet_data = pcap_file_parser.get_specific_packet(packet_num)
+    packet_data, packet_seconds_since_epco_time_stamp = pcap_file_parser.get_specific_packet(packet_num)
     packet_protocol_type = main_obj.get_packet_igp_protocol_type(packet_data)
     if packet_protocol_type is not None:
         print(func_name + "the packet is of type:" + packet_protocol_type)
+        print(func_name + "the packet time stamp in seconds since epoc is:" + str(packet_seconds_since_epco_time_stamp))
+
+    main_obj.get_packet_time(packet_data)
 
     print(func_name + "end")
