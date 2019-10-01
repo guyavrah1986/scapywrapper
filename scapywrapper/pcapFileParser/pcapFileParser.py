bee42f490651c6b974c1f2fb90f1d3f1cc57e86c
Guy Avraham <guyavrah1986@gmail.com>
2019-10-01 12:24:59 +0300

    PCAP_FILE_PARSER:changed the return value to return also the original time stamp of the packet (form the meta_data field).

--- scapywrapper/pcapFileParser/pcapFileParser.py
+++ scapywrapper/pcapFileParser/pcapFileParser.py
@@ -13,10 +13,12 @@ class PcapFileParser(Loggable):
         print(self.log_me() + "trying to extract packet number:" + str(packet_number))
         count = 0
         for (pkt_data, pkt_metadata) in RawPcapReader(self.pcap_file_name):
+            print(self.log_me() + "packet meta data:" + str(pkt_metadata))
+            packet_seconds_since_epoc_time_stamp = pkt_metadata.sec
+            print(self.log_me() + "packet meta seconds since epoc:" + str(packet_seconds_since_epoc_time_stamp))
             count += 1
-            print(self.log_me() + "extracting packet[" + str(count) + "]")
             if count == packet_number:
-                return pkt_data
+                return pkt_data, packet_seconds_since_epoc_time_stamp
 
         print(self.log_me() + "packet number:" + str(packet_number) + " does not exist in this pcap file")
         return None
